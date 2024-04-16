91341575c7212b3c64bfafc656627dca1f98a68f
webdevpro23 <166000397+webdevpro23@users.noreply.github.com>
2024-04-16 12:16:51 +0000

    data types done

--- 02_basics/test2.js
+++ 02_basics/test2.js
@@ -1,1 +1,12 @@
-console.log("web development prooooooo")
+const acccountId = 12345
+let accountEmail = "shavezali@gmail.com"
+var accountPassword = "121212121"
+accountCity = "uttarpradesh"
+
+
+console.table([acccountId,accountEmail,accountPassword,accountCity]);
+accountPassword = "234567"
+accountEmail = "shavezhcyahoo.com"
+accountCity = "lucknow"
+
+console.table([acccountId,accountEmail,accountPassword,accountCity]);
---
+++ 03_datatypes/datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+//alert (3+3) // we are using nodeJs not browser
+console.log(3+
+    3) // code readibilty should be high 
+
+    console.log("shavez")
+
+    let name = "shavez" 
+    let age = 18
+    let loggeddIn = true
+    let state;
+    //number => 2 to power 53  (range)
+     // bigint
+     //string => "" (range)
+     // boolean => true or false
+
+     //null => standalone value
+     // undefined =>
+     // symbol => unique
+
+
+
+
+
+
+// object
+
+console.log((typeof undefined))// outupt= undefined
+console.log(typeof null )// outuput= object
